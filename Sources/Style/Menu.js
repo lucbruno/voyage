4c36b306c68f50f47d93ad03b7d27bfb990aecd9
aurelie chenevoy <chenevoy.aurelie@gmail.com>
2016-05-23 23:21:08 -0700

    Footer and slide Menu

---
+++ Sources/Style/Menu.js
@@ -0,0 +1,13 @@
+
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+    showLeftPush = document.getElementById( 'showLeftPush' ),
+    body = document.body;
+
+
+showLeftPush.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( body, 'cbp-spmenu-push-toright' );
+    classie.toggle( menuLeft, 'cbp-spmenu-open' );
+    
+};
+
